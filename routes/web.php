a21ad00e551b4570d5a71ef2200dc774a146c16d
iliescumichele <iliescumichele@gmail.com>
2022-07-05 16:53:07 +0200

    creato cartelle per guest e admin con i relativi blade, corretto rotte view

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guest.welcome');
+}) -> name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'HomeController@index')->name('index');
