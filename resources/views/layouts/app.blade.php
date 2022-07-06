76779484e705db3c0e234d4951c98ac7c97d4214
iliescumichele <iliescumichele@gmail.com>
2022-07-07 00:48:42 +0200

    creato un link all’elenco dei post nel menu admin accessibile solo all’utente loggato

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,11 @@
                     Boolpress - Sito Pubblico
                 </a>
 
+                @auth
+                    <a class="navbar-brand" href="{{ route('admin.posts.index') }}">
+                        Link 'Vedi tutti i post'
+                    </a>
+                @endauth
                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
